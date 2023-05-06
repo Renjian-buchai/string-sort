dd58b24895ca2dbd9a2414cce236f4352c606170
Gahjouyooj <tyiwei0@gmail.com>
2023-05-06 14:01:20 +0800

    Chore: Added README.md Feat: Allowed user to select whether to trim spaces

---
+++ README.md
@@ -0,0 +1,41 @@
+# String Sort
+
+## About this project
+
+Command line application to sort a string by alphabetical order.
+
+## Examples
+
+Sorting a sentence
+
+```ps
+C:> strsort.exe The sun is a deadly lazer
+```
+
+Output:
+
+```ps
+      aaaddeeehillnrsstuyz
+```
+
+Sorting a sentence and trimming spaces
+
+```ps
+C:> strsort.exe The sun is a deadly lazer -T
+```
+
+Output:
+
+```ps
+aaaddeeehillnrsstuyz
+```
+
+## Possible uses
+
+I don't know, either.
+
+## Flags
+
+Flag `-T`:
+
+- Trims spaces after sorting
--- src.cc
+++ src.cc
@@ -1,17 +1,35 @@
 #include <algorithm>
+#include <array>
+#include <cctype>
 #include <iostream>
+#include <iterator>
 #include <string>
 
 int main(int argc, char** argv) {
-  std::string total = "";
+  // Flags:
+  // 0: trim spaces
+  std::array<bool, 1> flags;
+  flags.fill(false);
 
-  for (int i = 1; i < argc; ++i) {
-    total += std::string(argv[i]) + " ";
+  std::string input_string;
+
+  for (int i = 1; i < argc; i++) {
+    if (std::string(argv[i]) == "-T") {
+      flags[0] = true;
+    } else {
+      (input_string += argv[i]) += " ";
+    }
   }
-  std::remove(total.begin(), total.end(), ' ');
-  std::sort(total.begin(), total.end());
 
-  std::cout << total;
+  if (flags[0]) {
+    input_string.erase(
+        std::remove_if(input_string.begin(), input_string.end(), ::isspace),
+        input_string.end());
+  }
+
+  std::sort(input_string.begin(), input_string.end());
+
+  std::cout << input_string << std::endl;
 
   return 0;
 }
